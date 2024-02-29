9333388f5d4bce638de77308d954d9ec650b1fca
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2024-02-29 11:26:06 +0100

    under construction added

---
+++ joraxplc-app/src/components/UnderConstruction.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UnderConstruction(props) {
+  return (
+    <div>
+      <img src={`/assets/Under-construction.jpg`} alt="under construction" />
+    </div>
+  );
+}
+
+export default UnderConstruction;
