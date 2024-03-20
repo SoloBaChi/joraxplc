8585af9adfa1f93ec0e9b3805b50ab1f81db2a27
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2024-03-20 15:52:38 +0100

    in progress one

---
+++ jorax-app/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import Main from "./components/Main";
+import LaunchCountDown from "./components/LaunchCountDown";
+
+
+function App() {
+  return (
+    <div className="any-class">
+      {/* <Main /> */}
+      {/* <UnderConstruction /> */}
+      <LaunchCountDown/>
+      {/* <CountdownTimer/> */}
+    </div>
+  );
+}
+
+export default App;
