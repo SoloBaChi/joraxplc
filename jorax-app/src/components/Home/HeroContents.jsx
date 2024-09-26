2343ab716f34a949c0c1ceb02bd92afff166a1e9
SoloBaChi <solobachi02@gmail.com>
2024-09-26 22:22:20 +0100

    added sliding images

--- jorax-app/src/components/Home/HeroContents.jsx
+++ jorax-app/src/components/Home/HeroContents.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+// load the image Data
+const imageData = [
+  `/assets/images/hero-image.svg`,
+  `/assets/fourset_litre.jpg`,
+  `/assets/hand-holding-palmoil.jpg`,
+  `/assets/woman-peeling-palmoil.jpg`,
+];
 
 function HeroContents(props) {
+
+  const[position,setPosition] = useState(0);
+
+  // During Mount using SIde Effects
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+     if(position === imageData.length - 1) setPosition(0);
+     else{
+      setPosition(prev => prev + 1)
+     }
+    },3000);
+    return () => clearInterval(interval);
+  },[position]);
   return (
     <section className="banner-section">
       <div className="flex-container-2">
@@ -12,9 +33,10 @@ function HeroContents(props) {
             journey to a healthier life.
           </p>
         </div>
+        {/* style={{background:`url(${imageData[position]}) no-repeat center/cover #f8f8f8`}}> */}
         <div className="flex-item">
-          <div className="img-container">
-            <img src={`/assets/images/hero-image.svg`} alt="hero icon" />
+          <div className="img-container" style={{background:`url(${imageData[position]}) no-repeat center/cover`}}>
+            {/* <img src={`/assets/images/hero-image.svg`} alt="hero icon" /> */}
           </div>
         </div>
       </div>
